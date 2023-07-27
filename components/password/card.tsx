a855f21e999a7a146dbf1534f714529238e8a22a
Joao <jvfr96@gmail.com>
2023-07-27 20:27:50 -0300

    feat: show password on card

--- components/password/card.tsx
+++ components/password/card.tsx
@@ -1,15 +1,74 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Password } from "@/domain/types/password";
+import showPasswordIcon from "@/public/showPassword.svg";
+import hidePasswordIcon from "@/public/hidePassword.svg";
+import copyIcon from "@/public/copy.svg";
+import Image from "next/image";
 
 interface CardProps {
   password: Password;
 }
 
 const Card: FunctionComponent<CardProps> = ({ password }: CardProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordToggle = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleCopyPassword = () => {
+    navigator.clipboard.writeText(password.password);
+  };
+
   return (
     <div className="flex flex-col p-2 max-h-full max-w-md rounded bg-slate-500">
       <p className="text-white">{password.name}</p>
       <p className="text-white">{password.url}</p>
+      <p className="text-white">User: {password.username}</p>
+      <div className="flex justify-between items-center">
+        {showPassword ? (
+          <p className="text-white">Pass: {password.password}</p>
+        ) : (
+          <input
+            type="password"
+            value={password.password}
+            className="text-white bg-transparent focus:outline-none"
+            readOnly
+          />
+        )}
+        <div className="flex flex-row">
+          {showPassword ? (
+            <Image
+              onClick={handlePasswordToggle}
+              className="cursor-pointer"
+              priority
+              height={24}
+              width={24}
+              src={showPasswordIcon}
+              alt="show password"
+            />
+          ) : (
+            <Image
+              onClick={handlePasswordToggle}
+              className="cursor-pointer"
+              priority
+              height={24}
+              width={24}
+              src={hidePasswordIcon}
+              alt="hide password"
+            />
+          )}
+          <Image
+            onClick={handleCopyPassword}
+            className="cursor-pointer"
+            priority
+            height={24}
+            width={24}
+            src={copyIcon}
+            alt="copy password"
+          />
+        </div>
+      </div>
     </div>
   );
 };
