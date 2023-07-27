147856f562e937008a3dca2cac4998bb416cc1b2
Joao <jvfr96@gmail.com>
2023-07-27 18:25:36 -0300

    fix: form

--- components/password/form.tsx
+++ components/password/form.tsx
@@ -53,107 +53,105 @@ const PasswordForm = (props: PasswordFormProps) => {
     }
   };
 
-  const passwordForm = () => {
-    return (
-      <div>
-        <h2 className="text-xl font-semibold mb-4">Create password</h2>
-        <form onSubmit={handleFormSubmit}>
-          <div className="mb-4">
-            <label htmlFor="url" className="block text-gray-700">
-              URL:
-            </label>
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4">Create password</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="mb-4">
+          <label htmlFor="url" className="block text-gray-700">
+            URL:
+          </label>
+          <input
+            type="text"
+            id="url"
+            className={`w-full p-2 border rounded ${
+              errors.url ? "border-red-500" : ""
+            }`}
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          {errors.url && (
+            <p className="text-red-500 text-sm mt-1">{errors.url}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={`w-full p-2 border rounded ${
+              errors.name ? "border-red-500" : ""
+            }`}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.name}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            className={`w-full p-2 border rounded ${
+              errors.username ? "border-red-500" : ""
+            }`}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errors.username && (
+            <p className="text-red-500 text-sm mt-1">{errors.username}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700">
+            Password:
+          </label>
+          <div className="relative">
             <input
-              type="text"
-              id="url"
+              type={showPassword ? "text" : "password"}
+              id="password"
               className={`w-full p-2 border rounded ${
-                errors.url ? "border-red-500" : ""
+                errors.password ? "border-red-500" : ""
               }`}
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            {errors.url && (
-              <p className="text-red-500 text-sm mt-1">{errors.url}</p>
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
             )}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700">
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              className={`w-full p-2 border rounded ${
-                errors.name ? "border-red-500" : ""
-              }`}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            {errors.name && (
-              <p className="text-red-500 text-sm mt-1">{errors.name}</p>
-            )}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700">
-              Username:
-            </label>
-            <input
-              type="text"
-              id="username"
-              className={`w-full p-2 border rounded ${
-                errors.username ? "border-red-500" : ""
-              }`}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {errors.username && (
-              <p className="text-red-500 text-sm mt-1">{errors.username}</p>
-            )}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">
-              Password:
-            </label>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                className={`w-full p-2 border rounded ${
-                  errors.password ? "border-red-500" : ""
-                }`}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {errors.password && (
-                <p className="text-red-500 text-sm mt-1">{errors.password}</p>
-              )}
-              <button
-                type="button"
-                className="absolute top-2 right-2 text-gray-600"
-                onClick={handlePasswordToggle}
-              >
-                {showPassword ? "Hide" : "Show"}
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-end">
             <button
               type="button"
-              className="mr-4 px-4 py-2 bg-gray-400 text-white rounded-md"
-              onClick={props.onClose}
+              className="absolute top-2 right-2 text-gray-600"
+              onClick={handlePasswordToggle}
             >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md"
-            >
-              Save
+              {showPassword ? "Hide" : "Show"}
             </button>
           </div>
-        </form>
-      </div>
-    );
-  };
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="mr-4 px-4 py-2 bg-gray-400 text-white rounded-md"
+            onClick={props.onClose}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PasswordForm;
